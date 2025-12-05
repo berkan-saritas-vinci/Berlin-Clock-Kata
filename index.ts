d78d10dc2daa32669d17cf5a2c957b4ea4315976
berkan-saritas-vinci <berkan.saritas@student.vinci.be>
2025-12-05 17:03:21 +0100

    test3

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
+import { addSeconds } from "date-fns/fp";
+
 interface BerlinOclock {
     second: number;
     minute: number;
     heure: number
 }
 
+
+const berlin = new Date();
+
+const Minute(): Number => {
+    
+}
