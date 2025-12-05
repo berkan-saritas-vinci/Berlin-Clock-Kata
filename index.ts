8120ba10ed647d933e16527df6324a949539a2c5
Storme Simon <simon.storme@student.vinci.be>
2025-12-05 17:25:51 +0100

    add clock object and continue function

--- index.ts
+++ index.ts
@@ -1,15 +1,28 @@
-import { addSeconds } from "date-fns/fp";
-
 interface BerlinOclock {
     second: number;
     minute: number;
-    heure: number
+    minuteMultipleCinq: number;
+    heure: number;
+    heureMultipleCinq: number;
 }
 
 
 const BerlinClock = new Date();
+const clock: BerlinOclock = {
+    second: 0,
+    minute: 0,
+    minuteMultipleCinq: 0,
+    heure: 0,
+    heureMultipleCinq: 0
+};
 
 function simpleMinute(horloge : Date){
-    const minutes = horloge.getMinutes;
+    const minutes = horloge.getMinutes();
+    if(minutes === 1){
+        return 1;
+    }
 }
+
+
+
 
